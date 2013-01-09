211758acc5ed5a9cb52072075624ba491abf5e3d
Mariusz Pilarek <pieczaro@gmail.com>
2013-01-09 17:59:29 +0100

    added missing files

---
+++ src/core/trackrecords.cpp
@@ -0,0 +1,418 @@
+#include "trackrecords.h"
+
+#include "../core/seasondata.h"
+
+TrackRecords::TrackRecords()
+{
+}
+
+
+bool TrackRecords::loadTrackRecords(QString fileName)
+{
+    QFile f(fileName);
+    if (f.open(QIODevice::ReadOnly))
+    {
+        QDataStream stream(&f);
+
+        char *tab;
+        stream >> tab;
+
+        QString sbuf(tab);
+        delete tab;
+
+        if (QString(sbuf) != "F1LT2_TR")
+        {
+            f.close();
+            return false;
+        }
+
+        trackRecords.clear();
+
+        int size;
+        stream >> size;
+        for (int i = 0; i < size; ++i)
+        {
+            Track track;
+            stream >> track.name;
+
+            int verSize;
+            stream >> verSize;
+
+            for (int j = 0; j < verSize; ++j)
+            {
+                TrackVersion tv;
+                stream >> tv.year;
+                stream >> tv.map;
+
+                QString time;
+                for (int k = 0; k < 2; ++k)
+                {
+                    stream >> time;
+                    tv.trackRecords[k].time = LapTime(time);
+                    stream >> tv.trackRecords[k].driver;
+                    stream >> tv.trackRecords[k].team;
+                    stream >> tv.trackRecords[k].year;
+                }
+
+                int tsSize;
+                stream >> tsSize;
+
+                for (int k = 0; k < tsSize; ++k)
+                {
+                    TrackWeekendRecords ts;
+
+                    stream >> ts.year;
+
+
+                    for (int w = 0; w < 4; ++w)
+                    {
+                        stream >> time;
+                        ts.sessionRecords[w].time = LapTime(time);
+                        stream >> ts.sessionRecords[w].driver;
+                        stream >> ts.sessionRecords[w].team;
+                        stream >> ts.sessionRecords[w].session;
+                    }
+
+                    int dsSize;
+                    stream >> dsSize;
+                    for (int w = 0; w < dsSize; ++w)
+                    {
+                        DriverWeekendRecords ds;
+
+                        stream >> ds.driver;
+                        stream >> ds.team;
+
+                        for (int x = 0; x < 5; ++x)
+                        {
+                            for (int y = 0; y < 4; ++y)
+                            {
+                                stream >> time;
+                                ds.sessionRecords[x][y].time = LapTime(time);
+                                stream >> ds.sessionRecords[x][y].session;
+                            }
+                        }
+
+                        ts.driverRecords.append(ds);
+                    }
+
+                    tv.trackWeekendRecords.append(ts);
+                }
+                track.trackVersions.append(tv);
+            }
+            qSort(track.trackVersions);
+            trackRecords.append(track);
+        }
+    }
+    else
+        return false;
+
+    return true;
+}
+
+bool TrackRecords::saveTrackRecords(QString fileName)
+{
+    QFile file(fileName);
+
+    if (file.open(QIODevice::WriteOnly))
+    {
+        QDataStream stream(&file);
+
+        const char tab[9] = "F1LT2_TR";
+        stream << tab;
+
+        stream << trackRecords.size();
+
+        for (int i = 0; i < trackRecords.size(); ++i)
+        {
+            Track &tr = trackRecords[i];
+            stream << tr.name;
+            stream << tr.trackVersions.size();
+
+            for (int j = 0; j < tr.trackVersions.size(); ++j)
+            {
+                TrackVersion &tv = tr.trackVersions[j];
+
+                stream << tv.year;
+                stream << tv.map;
+
+                for (int k = 0; k < 2; ++k)
+                {
+                    stream << tv.trackRecords[k].time;
+                    stream << tv.trackRecords[k].driver;
+                    stream << tv.trackRecords[k].team;
+                    stream << tv.trackRecords[k].year;
+                }
+
+                stream << tv.trackWeekendRecords.size();
+
+                for (int k = 0; k < tv.trackWeekendRecords.size(); ++k)
+                {
+                    TrackWeekendRecords ts = tv.trackWeekendRecords[k];
+
+                    stream << ts.year;
+
+                    for (int w = 0; w < 4; ++w)
+                    {
+                        stream << ts.sessionRecords[w].time;
+                        stream << ts.sessionRecords[w].driver;
+                        stream << ts.sessionRecords[w].team;
+                        stream << ts.sessionRecords[w].session;
+                    }
+
+                    stream << ts.driverRecords.size();
+
+                    for (int w = 0; w < ts.driverRecords.size(); ++w)
+                    {
+                        DriverWeekendRecords ds = ts.driverRecords[w];
+
+                        stream << ds.driver;
+                        stream << ds.team;
+
+                        for (int x = 0; x < 5; ++x)
+                        {
+                            for (int y = 0; y < 4; ++y)
+                            {
+                                stream << ds.sessionRecords[x][y].time;
+                                stream << ds.sessionRecords[x][y].session;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    else
+        return false;
+
+    return true;
+}
+
+void TrackRecords::getCurrentTrackRecords(TrackWeekendRecords **twr, TrackVersion **tv)
+{
+    LTEvent event = EventData::getInstance().getEventInfo();
+
+    for (int i = 0; i < trackRecords.size(); ++i)
+    {
+        if (trackRecords[i].name == event.eventPlace)
+        {
+            int j = 0;
+            for (; j < trackRecords[i].trackVersions.size(); ++j)
+            {
+                if (trackRecords[i].trackVersions[j].year > event.fpDate.year())
+                    break;
+
+                TrackWeekendRecords *t = &trackRecords[i].trackVersions[j].getTrackWeekendRecords(event.fpDate.year());
+
+                if (*t != TrackWeekendRecords::null())
+                {
+                    *twr = t;
+                    *tv = &trackRecords[i].trackVersions[j];
+                    return;
+                }
+            }
+            //if no records from this year were found, they have to be added
+            --j;
+
+            TrackWeekendRecords tr;
+            tr.year = event.fpDate.year();
+            trackRecords[i].trackVersions[j].trackWeekendRecords.append(tr);
+
+            qSort(trackRecords[i].trackVersions[j].trackWeekendRecords);
+
+            TrackWeekendRecords *t = &trackRecords[i].trackVersions[j].getTrackWeekendRecords(event.fpDate.year());
+            if (*t != TrackWeekendRecords::null())
+            {
+                *twr = t;
+                *tv = &trackRecords[i].trackVersions[j];
+                return;
+            }
+        }
+    }
+}
+
+QString TrackRecords::getCurrentSessionAsString()
+{
+    EventData &ed = EventData::getInstance();
+    QString tmp;
+    switch (ed.getEventType())
+    {
+        case LTPackets::PRACTICE_EVENT:
+            tmp = "FP" + QString::number(ed.getFPNumber());
+            break;
+
+        case LTPackets::QUALI_EVENT:
+            tmp = "Q" + QString::number(ed.getQualiPeriod());
+            break;
+
+        case LTPackets::RACE_EVENT:
+            tmp = "R";
+            break;
+    }
+    return tmp;
+}
+
+eSession TrackRecords::getSessionFromString(QString ses)
+{
+    if (ses == "FP1")
+        return FP1;
+
+    if (ses == "FP2")
+        return FP2;
+
+    if (ses == "FP3")
+        return FP3;
+
+    if (ses.contains("Q"))
+        return QUALI;
+
+    if (ses == "RACE")
+        return RACE;
+
+    return FP1;
+}
+
+eSession TrackRecords::getCurrentSession()
+{
+    EventData &ed = EventData::getInstance();
+    switch (ed.getEventType())
+    {
+        case LTPackets::PRACTICE_EVENT:
+            return (eSession)(ed.getFPNumber() - 1);
+
+        case LTPackets::QUALI_EVENT:
+            return QUALI;
+
+        case LTPackets::RACE_EVENT:
+            return RACE;
+    }
+
+    return FP1;
+}
+
+void TrackRecords::gatherSessionRecords(bool withDriverRecords)
+{
+    EventData &ed = EventData::getInstance();
+    TrackWeekendRecords *twr = 0;
+    TrackVersion *tv = 0;
+    getCurrentTrackRecords(&twr, &tv);
+
+    if (twr != 0 && tv != 0)
+    {
+        for (int i = 0; i < NUM_SECTORS; ++i)
+        {
+            LapTime sessionRecord = ed.getSessionRecords().getSectorRecord(i+1).getTime();
+            if (i == 3)
+                sessionRecord = ed.getSessionRecords().getFastestLap().getTime();
+
+            qDebug() << sessionRecord.toString() << twr->sessionRecords[i].time.toString();
+            if (sessionRecord.isValid() && sessionRecord < twr->sessionRecords[i].time)
+            {
+                twr->sessionRecords[i].time = sessionRecord;
+
+                int no;
+                if (i < 3)
+                {
+                    twr->sessionRecords[i].driver = ed.getSessionRecords().getSectorRecord(i+1).getDriverName();
+                    no = ed.getSessionRecords().getSectorRecord(i+1).getNumber();
+                }
+                else
+                {
+                    twr->sessionRecords[i].driver = ed.getSessionRecords().getFastestLap().getDriverName();
+                    no = ed.getSessionRecords().getFastestLap().getNumber();
+                }
+
+
+                twr->sessionRecords[i].team = SeasonData::getInstance().getTeamName(no);
+                twr->sessionRecords[i].session = getCurrentSessionAsString();
+            }
+        }
+
+        if (ed.getEventType() != LTPackets::PRACTICE_EVENT)
+        {
+            LapTime sessionRecord = ed.getSessionRecords().getFastestLap().getTime();
+
+            eTrackRecord currEvent = QUALI_RECORD;
+            if (ed.getEventType() == LTPackets::RACE_EVENT)
+                currEvent = RACE_RECORD;
+
+            if (sessionRecord.isValid() && sessionRecord < tv->trackRecords[currEvent].time)
+            {
+                tv->trackRecords[currEvent].time = sessionRecord;
+                tv->trackRecords[currEvent].driver = ed.getSessionRecords().getFastestLap().getDriverName();
+                int no = ed.getSessionRecords().getFastestLap().getNumber();
+                tv->trackRecords[currEvent].team = SeasonData::getInstance().getTeamName(no);
+                tv->trackRecords[currEvent].year = QString::number(ed.getEventInfo().fpDate.year());
+            }
+        }
+
+        if (withDriverRecords)
+            gatherDriverRecords(twr, tv);
+    }
+}
+
+void TrackRecords::gatherDriverRecords(TrackWeekendRecords *twr, TrackVersion *tv)
+{
+    EventData &ed = EventData::getInstance();
+    if (twr != 0 && tv != 0)
+    {
+        int k = 0;
+        for (int i = 0; i < 30; ++i)
+        {
+            DriverData *dd = ed.getDriverDataPtr(i+1);
+
+            if (dd == 0)
+                continue;
+
+            DriverWeekendRecords dsRecords;
+
+            if (k < twr->driverRecords.size())
+            {
+                dsRecords = twr->driverRecords[k];
+
+                //to keep the records of test drivers too, we have to insert them between usual drivers
+                if (dsRecords.driver != dd->getDriverName())
+                    ++k;
+
+                if (k < twr->driverRecords.size())
+                {
+                    dsRecords = twr->driverRecords[k];
+                    if (dsRecords.driver != dd->getDriverName())
+                        twr->driverRecords.insert(k, dsRecords);
+                }
+            }
+
+            dsRecords.driver = dd->getDriverName();
+            dsRecords.team = SeasonData::getInstance().getTeamName(i+1);
+
+            for (int j = 0; j < 4; ++j)
+            {
+                LapTime driverRecord = dd->getSessionRecords().getBestSector(j+1).first;
+                if (j == 3)
+                {
+                    driverRecord = dd->getSessionRecords().getBestLap().getTime();
+                }
+
+                if (driverRecord.isValid() && driverRecord <= dsRecords.sessionRecords[getCurrentSession()][j].time)
+                {                    
+                    dsRecords.sessionRecords[getCurrentSession()][j].time = driverRecord;
+                    dsRecords.sessionRecords[getCurrentSession()][j].session = getCurrentSessionAsString();
+
+                    if (ed.getEventType() == LTPackets::QUALI_EVENT)
+                    {
+                        dsRecords.sessionRecords[getCurrentSession()][j].session = "Q" +
+                                QString::number(dd->getSessionRecords().getBestLap().getQualiLapExtraData().getQualiPeriod());
+                    }
+                }                
+            }
+
+            if (twr->driverRecords.size() <= k)
+                twr->driverRecords.append(dsRecords);
+            else
+                twr->driverRecords[k] = dsRecords;
+
+            ++k;
+        }
+    }
+}
+
+//TrackRecordsAtom TrackRecords::nullRecord;
---
+++ src/core/trackrecords.h
@@ -0,0 +1,399 @@
+#ifndef TRACKRECORDS_H
+#define TRACKRECORDS_H
+
+#include "../core/lapdata.h"
+#include "../core/eventdata.h"
+
+const int NUM_SESSIONS=5;       //FP1, FP2, FP3, Quali, Race
+const int NUM_SECTORS=4;        //3 sectors + lap time
+
+enum eTrackRecord
+{
+    QUALI_RECORD = 0,
+    RACE_RECORD
+};
+
+enum eSectorRecord
+{
+    S1_RECORD = 0,
+    S2_RECORD,
+    S3_RECORD,
+    TIME_RECORD
+};
+
+enum eSession
+{
+    FP1 = 0,
+    FP2,
+    FP3,
+    QUALI,
+    RACE
+};
+
+struct Record
+{
+    LapTime time;
+    QString driver;
+    QString team;
+    QString year;
+    QString session;
+};
+
+struct DriverWeekendRecords
+{
+    QString driver;
+    QString team;
+    Record sessionRecords[NUM_SESSIONS][NUM_SECTORS];
+
+    DriverWeekendRecords &operator=(const DriverWeekendRecords &dr)
+    {
+        if (this == &dr)
+            return *this;
+
+        driver = dr.driver;
+        team = dr.team;
+
+        for (int i = 0; i < NUM_SESSIONS; ++i)
+        {
+            for (int j = 0; j < NUM_SECTORS; ++j)
+                sessionRecords[i][j] = dr.sessionRecords[i][j];
+        }
+
+        return *this;
+    }
+
+    Record getWeekendRecord(eSectorRecord sector) const
+    {
+        Record rec = sessionRecords[0][sector];
+
+        for (int i = 1; i < NUM_SESSIONS; ++i)
+        {
+           if (sessionRecords[i][sector].time < rec.time)
+               rec = sessionRecords[i][sector];
+        }
+        return rec;
+    }
+};
+
+struct TrackWeekendRecords
+{
+    int year;
+    Record sessionRecords[NUM_SECTORS];
+
+    QList<DriverWeekendRecords> driverRecords;
+
+    static TrackWeekendRecords &null()
+    {
+        static TrackWeekendRecords nullTR;
+        return nullTR;
+    }
+
+    bool operator==(const TrackWeekendRecords &tr) const
+    {
+        return year == tr.year;
+    }
+
+    bool operator!=(const TrackWeekendRecords &tr) const
+    {
+        return year != tr.year;
+    }
+
+    bool operator < (const TrackWeekendRecords &twr) const
+    {
+        return year < twr.year;
+    }
+
+
+    TrackWeekendRecords &operator=(const TrackWeekendRecords &tr)
+    {
+        if (this == &tr)
+            return *this;
+
+        year = tr.year;
+
+        for (int i = 0; i < NUM_SECTORS; ++i)
+        {
+            sessionRecords[i] = tr.sessionRecords[i];
+        }
+        driverRecords = tr.driverRecords;
+
+        return *this;
+    }
+
+    DriverWeekendRecords getDriverRecords(QString driver)
+    {
+        for (int i = 0; i < driverRecords.size(); ++i)
+        {
+            if (driver == driverRecords[i].driver)
+                return driverRecords[i];
+        }
+
+        return DriverWeekendRecords();
+    }
+};
+
+struct TrackVersion
+{
+    QPixmap map;
+    int year;
+    Record trackRecords[2];
+    QList<TrackWeekendRecords> trackWeekendRecords;
+
+    bool operator < (const TrackVersion &tr) const
+    {
+        return year < tr.year;
+    }
+
+    bool operator == (const TrackVersion &tr) const
+    {
+        return (year == tr.year);
+    }
+
+    bool operator != (const TrackVersion &tr) const
+    {
+        return (year != tr.year);
+    }
+
+    TrackWeekendRecords &operator[](int idx)
+    {
+        if (idx >= 0 && idx < trackWeekendRecords.size())
+            return trackWeekendRecords[idx];
+
+        return TrackWeekendRecords::null();
+    }
+
+    TrackVersion &operator=(const TrackVersion &tr)
+    {
+        if (this == &tr)
+            return *this;
+
+        year = tr.year;
+        map = tr.map;
+
+        for (int i = 0; i < 2; ++i)
+            trackRecords[i] = tr.trackRecords[i];
+
+        trackWeekendRecords = tr.trackWeekendRecords;
+
+        return *this;
+    }
+
+    TrackWeekendRecords &getTrackWeekendRecords(int year)
+    {
+        for (int i = 0; i < trackWeekendRecords.size(); ++i)
+        {
+            if (trackWeekendRecords[i].year == year)
+                return trackWeekendRecords[i];
+        }
+        return TrackWeekendRecords::null();
+    }
+
+    TrackWeekendRecords &removeTrackWeekendRecords(TrackWeekendRecords &tr)
+    {
+        int idx = trackWeekendRecords.indexOf(tr);
+
+        if (idx != -1)
+        {
+            trackWeekendRecords.takeAt(idx);
+
+            if (trackWeekendRecords.isEmpty())
+                return TrackWeekendRecords::null();
+
+            return trackWeekendRecords.first();
+        }
+        return tr;
+    }
+
+    static TrackVersion &null()
+    {
+        static TrackVersion tvNull;
+        return tvNull;
+    }
+
+};
+
+struct Track
+{
+    QString name;
+    QList<TrackVersion> trackVersions;
+
+    TrackVersion &operator[](int idx)
+    {
+        if (idx >= 0 && idx < trackVersions.size())
+            return trackVersions[idx];
+
+        return TrackVersion::null();
+    }
+
+    bool operator < (const Track &tr) const
+    {
+        return name < tr.name;
+    }
+
+    static Track &null()
+    {
+        static Track nullTrack;
+        return nullTrack;
+    }
+
+    bool operator!=(const Track &tr)
+    {
+        return (name != tr.name || trackVersions.size() != tr.trackVersions.size());
+    }
+
+    TrackVersion &getTrackVersion(int year)
+    {
+        for (int i = 0; i < trackVersions.size(); ++i)
+        {
+            if (trackVersions[i].year == year)
+                return trackVersions[i];
+        }
+        return TrackVersion::null();
+    }
+
+    TrackVersion &removeTrackVersion(TrackVersion &tv)
+    {
+        int idx = trackVersions.indexOf(tv);
+
+        if (idx != -1)
+        {
+            trackVersions.takeAt(idx);
+
+            if (trackVersions.isEmpty())
+                return TrackVersion::null();
+
+            return trackVersions.first();
+        }
+        return tv;
+    }
+};
+
+class TrackRecords
+{
+public:   
+
+    static TrackRecords &getInstance()
+    {
+        static TrackRecords instance;
+        return instance;
+    }
+
+    bool loadTrackRecords(QString fileName);
+    bool saveTrackRecords(QString fileName);
+
+    QList<Track> &getTrackRecords() { return trackRecords; }
+
+    Track &operator[](int idx)
+    {
+        if (idx >= 0 && idx < trackRecords.size())
+            return trackRecords[idx];
+
+        return Track::null();
+    }
+
+    void getCurrentTrackRecords(TrackWeekendRecords **twr, TrackVersion **tv);
+    void gatherSessionRecords(bool withDriverRecords = false);
+    void gatherDriverRecords(TrackWeekendRecords *twr, TrackVersion *tv);
+
+    QStringList getTrackList()
+    {
+        QStringList sList;
+
+        for (int i = 0; i < trackRecords.size(); ++i)
+        {
+            QString trackName = trackRecords[i].name;
+            sList.append(trackName);
+        }
+
+        sList.sort();
+
+        return sList;
+    }
+
+    QString getCurrentSessionAsString();
+    eSession getCurrentSession();
+    eSession getSessionFromString(QString ses);
+
+private:    
+
+    TrackRecords();
+    QList<Track> trackRecords;
+
+};
+
+
+
+//struct TrackRecordsAtom
+//{
+//    QString trackName;
+//    QPixmap trackMap;
+//    Record trackRecords[2];
+//    Record sessionRecords[4];
+//    QList<DriverWeekendRecords> driverRecords;
+
+////    Record getDriverSessionRecord(QString driver, eSectorRecord sector, eSession session)
+////    {
+////        QList<Record> foundRecords;
+////        for (int i = 0; i < driverRecords.size(); ++i)
+////        {
+////            if (driver == driverRecords[i].driver)
+////            {
+////                if (session == WEEKEND)
+////                    foundRecords.append(driverRecords[i].sessionRecords[sector]);
+////                else
+////                {
+////                    if (driverRecords[i].sessionRecords[sector].session == "FP1" && session == FP1)
+////                        return driverRecords[i].sessionRecords[sector];
+
+////                    if (driverRecords[i].sessionRecords[sector].session == "FP2" && session == FP2)
+////                        return driverRecords[i].sessionRecords[sector];
+
+////                    if (driverRecords[i].sessionRecords[sector].session == "FP3" && session == FP3)
+////                        return driverRecords[i].sessionRecords[sector];
+
+////                    if (driverRecords[i].sessionRecords[sector].session == "Race" && session == Race)
+////                        return driverRecords[i].sessionRecords[sector];
+
+////                    if (driverRecords[i].sessionRecords[sector].session.contains("Q") && session == Quali)
+////                        return driverRecords[i].sessionRecords[sector];
+////                }
+////            }
+////        }
+
+////        //search for a weekend record
+////        if (session == WEEKEND && foundRecords.size() > 0)
+////        {
+////            Record rec = foundRecords[0];
+
+////            for (int i = 1; i < foundRecords.size(); ++i)
+////            {
+////                if (foundRecords[i].time < rec.time)
+////                    rec = foundRecords[i];
+////            }
+////            return rec;
+////        }
+////        return Record();
+////    }
+
+//    static TrackRecordsAtom &null()
+//    {
+//        static TrackRecordsAtom nullTR;
+//        return nullTR;
+//    }
+
+//    bool operator < (const TrackRecordsAtom &tr) const
+//    {
+//        return trackName < tr.trackName;
+//    }
+
+//    bool operator == (const TrackRecordsAtom &tr) const
+//    {
+//        return (trackName == tr.trackName);
+//    }
+
+//    bool operator != (const TrackRecordsAtom &tr) const
+//    {
+//        return (trackName != tr.trackName);
+//    }
+//};
+#endif // TRACKRECORDS_H
